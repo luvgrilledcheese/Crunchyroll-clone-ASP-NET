95265a3df4cb9beb8f799276d9992de7d97b9a2b
Richi Tan <e1629525@cmaisonneuve.qc.ca>
2020-02-10 15:51:48 -0500

    e1629525
    Ajout de la classe e1629525

---
+++ tp1e18/Models/DataModels/e1629525.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tp1e18.Models.DataModels
+{
+    public class e1629525
+    {
+        public int e1629525Id { get; set; }
+    }
+}
