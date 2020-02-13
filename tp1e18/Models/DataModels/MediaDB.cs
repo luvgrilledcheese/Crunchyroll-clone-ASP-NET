b9ebb909e3413ca6aef9d56dd7fd32021f38dd79
Richi Tan <e1629525@cmaisonneuve.qc.ca>
2020-02-12 21:05:15 -0500

    Created database Media
    Media.mdf

---
+++ tp1e18/Models/DataModels/MediaDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace tp1e18.Models.DataModels
+{
+    public class MediaDb : DbContext
+    {
+        public MediaDb() : base(nameOrConnectionString: "CSEcoleDb")
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+            // Database.SetInitializer(new MediaInitializer());
+        }
+        public Dbset<Serie> Serie { get; set; }
+    }
+}
