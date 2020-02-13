2df0bc322403e5c882e0492da79a85115b8f8840
Andy Luong <e1671879@cmaisonneuve.qc.ca>
2020-02-12 22:17:52 -0500

    Media

--- tp1e18/Models/DataModels/Media.cs
+++ tp1e18/Models/DataModels/Media.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace tp1e18.Models.DataModels
 {
-    public class Media
+    public partial class Media : DbContext
     {
+        public Media()
+            : base("name=Media")
+        {
+            Database.SetInitializer(new MediaInitializer());
+        }
+
+        public virtual DbSet<Studio> Studio { get; set; }
+        public virtual DbSet<Serie> Serie { get; set; }
+        public virtual DbSet<Saison> Saison { get; set; }
+        public virtual DbSet<Episode> Episode { get; set; }
+
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
     }
 }
